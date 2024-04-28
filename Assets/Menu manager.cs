b2055099166e1347f39efb286c58e4aa1087812c
ProMen10 <168337697+ProMen10@users.noreply.github.com>
2024-04-28 12:57:29 +0300

    123
    ///

---
+++ Assets/Menu manager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject settingsPanel;
+
+    public void PlayGane()
+    {
+        Application.LoadLevel("Game");
+    }
+
+    public void ExitGane()
+    {
+        Application.Quit();
+    }
+
+    public void SettingsPanel()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+
+    public void Exit()
+    {
+        settingsPanel.SetActive(false);
+    }
+}
